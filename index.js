96cd27a4a93f9741c180309cb05edcc6e81ce802
Programmerbikash <bikash19081998dey@gmail.com>
2022-10-21 20:40:22 +0600

    clear

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./categories.json');
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
   res.send('News api running!')
@@ -13,6 +14,28 @@ app.get('/', (req, res) => {
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews); 
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    // console.log(req.params)
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
 })
 
 app.listen(port, () => {
